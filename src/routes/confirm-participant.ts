c44b5bf3bdb397c74e4d62196189725a7f294dac
Bruno Pereira <brunoleandropereira@hotmail.com>
2024-07-30 18:56:02 -0300

    environment variables

--- src/routes/confirm-participant.ts
+++ src/routes/confirm-participant.ts
@@ -3,6 +3,7 @@ import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
 import { ClientError } from "../errors/client-error";
+import { env } from "../env";
 
 
 export async function confirmParticipants(app: FastifyInstance){
@@ -27,7 +28,7 @@ export async function confirmParticipants(app: FastifyInstance){
         }
 
         if (participant.is_confirmed) {
-            return reply.redirect(`http://localhost:3000/trips/${participant.trip_id}`)
+            return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_id}`)
         }
 
         await prisma.participant.update ({
@@ -36,6 +37,6 @@ export async function confirmParticipants(app: FastifyInstance){
         })
 
 
-        return reply.redirect(`http://localhost:3000/trips/${participant.trip_id}`)
+        return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_id}`)
     })
 }
